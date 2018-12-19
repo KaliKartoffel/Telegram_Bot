2da32b650fc53a7b105c492279f1ddcfcf997fb9
KaliKartoffel <edthofermatthias@gmail.com>
2018-12-19 21:01:34 +0100

    Added name dictionary and small features

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -8,12 +8,14 @@ using Telegram.Bot.Types.InlineQueryResults;
 using Telegram.Bot.Types.ReplyMarkups;
 using System.IO;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Awesome
 {
     class Program
     {
         static ITelegramBotClient botClient;
+        static IDictionary<long, string> userNames = new Dictionary<long, string>();
 
         static void Main()
         {
@@ -22,26 +24,70 @@ namespace Awesome
             var me = botClient.GetMeAsync().Result;
             Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
 
+            
+            var lines = File.ReadAllLines("..\\..\\..\\Files\\userNames.txt");
+            foreach (string line in lines)
+            {
+                string[] keyValue = line.Split(":");
+                userNames.Add(Convert.ToInt64(keyValue[0]), keyValue[1]);
+            }
+
             botClient.OnMessage += Bot_OnMessage;
 
             botClient.StartReceiving();
-            for (;;) Thread.Sleep(int.MaxValue);
+            ConsoleInput();
+        }
+
+        static void ConsoleInput()
+        {
+            string[] commands = new string[] { "stop" };
+            for (;;)
+            {
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "stop":
+                        using (StreamWriter file = new StreamWriter("..\\..\\..\\Files\\userNames.txt"))
+                            foreach (var entry in userNames)
+                                file.WriteLine("{0}:{1}", entry.Key, entry.Value);
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("I don't get what you are saying. Available commands: " + String.Join(", ", commands));
+                        break;
+                }
+            }
         }
 
         static async void Bot_OnMessage(object sender, MessageEventArgs e)
         {
-            string[] commands = new string[] {"/test"};
+            string[] commands = new string[] {"/test", "/name"};
             var message = e.Message;
             if (message.Text == null || message.Type != MessageType.Text) throw new System.ArgumentException("message is either 0 or != Text"); 
 
-            Console.WriteLine($"Received a text message in chat {message.Chat.Id} with the Text {message.Text}.");
-
+            Console.WriteLine($"Received a text message in chat {message.Chat.Id} with the Text \"{message.Text}\" from {message.Chat.FirstName}.");
             switch (message.Text.Split(" ").First())
             {
                 case "/test":
                     await botClient.SendTextMessageAsync(
                         chatId: message.Chat,
                         text: $"Testing if I work?"
+                    );
+                    break;
+                case "/name":
+                    string[] splitMsg = message.Text.Split(" ", 2);
+                    if (splitMsg.Length == 1)
+                    {
+                        await botClient.SendTextMessageAsync(
+                            chatId: message.Chat,
+                            text: $"Your Name is {userNames[message.Chat.Id]}"
+                        );
+                        break;
+                    }
+                    userNames[message.Chat.Id] = splitMsg[1];
+                    await botClient.SendTextMessageAsync(
+                        chatId: message.Chat,
+                        text: $"Your new Name is {splitMsg[1]}"
                     );
                     break;
                 default:
